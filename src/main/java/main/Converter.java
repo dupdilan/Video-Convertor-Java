48b877db0aab3ba4704fea3a62bfca06165196d0
lakshithasadeeshana <sadeeshana1995@gmail.com>
2020-07-16 11:49:00 +0530

    update the Converter class to mp4tomkv convert and fixed FileListener

--- src/main/java/main/Converter.java
+++ src/main/java/main/Converter.java
@@ -38,6 +38,44 @@ public class Converter {
 
     }
 
+    //converting method mp4 file to mkv
+    public void mp4toMKV(String name){
+
+        try {
+            File source = new File("D:\\media\\mp4tomkv\\"+name);
+            File target = new File("D:\\media-converted\\mp4tomkv-converted\\"+name+".mkv");
+
+            // convert the Audio parts
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("libmp3lame");
+            audio.setBitRate(new Integer(64000));
+            audio.setChannels(new Integer(1));
+            audio.setSamplingRate(new Integer(22050));
+
+            //convert the Vedio parts
+            VideoAttributes video = new VideoAttributes();
+            video.setCodec("mpeg4");
+            video.setBitRate(new Integer(160000));
+            video.setFrameRate(new Integer(15));
+            video.setSize(new VideoSize(400, 300));
+
+            //Encoding attributes
+            EncodingAttributes attrs = new EncodingAttributes();
+            attrs.setFormat("matroska");
+            attrs.setAudioAttributes(audio);
+            attrs.setVideoAttributes(video);
+
+            //Encordr method
+            Encoder encoder = new Encoder();
+
+            encoder.encode(new MultimediaObject(source), target, attrs);
+
+        } catch (Exception ex) {
+            boolean succeeded = false;
+        }
+
+    }
+
 
 
 }
